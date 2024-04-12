8b18f1bc0214712c8ef15b96cdfb12dc86b63ed5
shyank akshar <akshar@shyankdev.us>
2024-04-13 01:14:52 +0530

    add slider ui component

--- src/components/SelectionEditor.vue
+++ src/components/SelectionEditor.vue
@@ -1,12 +1,20 @@
 <template>
+  <div class="flex justify-between rounded-lg p-4 m-4">
+    <span class="text-white">font weight slider</span>
 
-    <div class="flex justify-evenly rounded-lg p-4 m-4">
-        <span class="text-white">font weight slider</span>
+    <!-- <h1 class="text-white">export </h1> -->
+    <span grow>
+      <VueSlider v-model="value" />
+      <p class="text-white">Value: {{ value }}</p>
+    </span>
 
-        <!-- <h1 class="text-white">export </h1> -->
-        <button class="text-white">
-            export with get text
-        </button>
-    </div>
-
+    <button class="text-white">export with get text</button>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref, type Ref } from "vue";
+import VueSlider from "vue-3-slider-component";
+
+const value: Ref<Number> = ref(0);
+</script>
