7146d54d0a8bc808ba294edff7bbe57b27ef9276
shyank akshar <akshar@shyankdev.us>
2024-04-13 01:08:13 +0530

    add editor section for text fondt weight slider

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <main class="h-screen w-screen bg-gray-900">
     <HeadComponent/>
-
+    <SelectionEditor/>
     <div class="p-4 m-4 rounded-lg bg-white">
       <TipTap/>
     </div>
+
+    
   
   </main>
 </template>
@@ -15,12 +17,6 @@ import { ref, type Ref } from "vue";
 import TipTap from "./components/Tiptap.vue";
 
 import HeadComponent from "./components/HeadComponent.vue";
+import SelectionEditor from "./components/SelectionEditor.vue";
 
-// const header = HeadComponent
-// export default {
-//   components: {
-//     HeadComponent,
-//   },
-// };
-// const count: Ref<number> = ref(1);
 </script>
---
+++ src/components/SelectionEditor.vue
@@ -0,0 +1,12 @@
+<template>
+
+    <div class="flex justify-evenly rounded-lg p-4 m-4">
+        <span class="text-white">font weight slider</span>
+
+        <!-- <h1 class="text-white">export </h1> -->
+        <button class="text-white">
+            export with get text
+        </button>
+    </div>
+
+</template>
